0f4a3b8f5bef2b74e699e75eec9e7fdbe451e626
David Bernard <david.bernard.31@gmail.com>
2011-07-26 21:45:56 +0200

    samples: upgrade scala version, java version, maven-scala-plugin, ...

--- samples/prj-scala-only/src/test/scala/samples/SpecsSampleTest.scala
+++ samples/prj-scala-only/src/test/scala/samples/SpecsSampleTest.scala
@@ -1,40 +1,16 @@
 package samples
 
-import org.specs._
+import org.specs2._
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
 
-//import org.specs.runner.JUnit4
-//class SpecsSampleTest extends JUnit4(SpecsSample)
-//object SpecsSample extends Specification with ScalaCheck {
-
-class SpecsSampleTest extends SpecificationWithJUnit  with ScalaCheck {
-
-//import org.junit.runner.RunWith
-//import org.specs.runner.{ JUnitSuiteRunner, JUnit }
-//@RunWith(classOf[JUnitSuiteRunner])
-//class SpecsSampleTest extends Specification with JUnit with ScalaCheck {
+@RunWith(classOf[JUnitRunner])
+class SpecsSampleTest extends mutable.Specification {
 
   "'hello world' has 11 characters" in {
      "hello world".size must be equalTo(11)
   }
   "'hello world' matches 'h.* w.*'" in {
      "hello world" must be matching("h.* w.*")
-  }
-  
-  "startsWith" verifies { (a: String, b: String) => (a + b).startsWith(a) }
-  "endsWith" verifies { (a: String, b: String) => (a + b).endsWith(b) }
-  
-  "These examples" should {
-    skip("those examples don't pass yet")
-    "be skipped" in { /*... */}
-    "be skipped2" in { /*... */}
-  }
-  
-  "my framework" should {
-    "work with Xxxx" in {
-      // skip the example if the DB2 connection is not available locally
-      false must beTrue.orSkipExample // alias orSkip
-      // use DB2Connection
-    }
-    /*...*/
   }
 }
