c2806af229c5c4fc2a73bd0fc2fb0bf4fbfc85d7
David Bernard <david.bernard.31@gmail.com>
2014-10-13 15:00:36 +0200

    prj-scala-only: update to scala 2.10

--- samples/prj-scala-only/src/test/scala/samples/ScalaTestFunSuite.scala
+++ samples/prj-scala-only/src/test/scala/samples/ScalaTestFunSuite.scala
@@ -1,36 +1,21 @@
 package samples
 
-import org.scalatest.BeforeAndAfterAll
 import _root_.org.scalatest.junit.JUnitSuite
 import _root_.org.junit.runner.RunWith
 import _root_.org.scalatest.junit.JUnitRunner
 import _root_.org.scalatest.FunSuite
-import org.scalatest.prop.Checkers
+
 
 @RunWith(classOf[JUnitRunner])
-class ScalaTestFunSuite extends FunSuite  with Checkers with BeforeAndAfterAll {
-  import scala.collection.mutable.Stack
-  // ...
-  
-  override def beforeAll(configMap: Map[String, Any]) {
-	 //require(false, "false")
-	 // fail("tt")
+class ScalaTestFunSuite extends FunSuite {
+
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size === 0)
   }
-  
-  test("pop is invoked on a non-empty stack") {
- 
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    val oldSize = stack.size
-    val result = stack.pop()
-    assert(result === 2)
-    assert(stack.size === oldSize - 1 )
-  }
- 
-  test("pop is invoked on an empty stack") (pending)
-  
-  test("concat list") {
-    check((a: List[Int], b: List[Int]) => a.size + b.size == (a ::: b).size )
+
+  test("Invoking head on an empty Set should produce NoSuchElementException") {
+    intercept[NoSuchElementException] {
+      Set.empty.head
+    }
   }
 }
--- samples/prj-scala-only/src/test/scala/samples/ScalaTestJUnitSuite.scala
+++ samples/prj-scala-only/src/test/scala/samples/ScalaTestJUnitSuite.scala
@@ -1,54 +1,30 @@
 package samples
 
 import org.scalatest.junit.JUnitSuite
-import org.scalatest.junit.ShouldMatchersForJUnit
 import scala.collection.mutable.ListBuffer
-import org.junit.Test
-import org.junit.Before
-import org.scalatest.prop.Checkers
+import _root_.org.junit.Test
+import _root_.org.junit.Before
 
-// following import generate NPE into eclipse
-//import org.scalacheck.Arbitrary._ 
-//import org.scalacheck.Prop._
-
-class ScalaTestJUnitSuite extends JUnitSuite with ShouldMatchersForJUnit with Checkers {
+class ScalaTestJUnitSuite extends JUnitSuite {
 
   var sb: StringBuilder = _
   var lb: ListBuffer[String] = _
 
-  @Before
-  def initialize() {
+  @Before def initialize() {
     sb = new StringBuilder("ScalaTest is ")
     lb = new ListBuffer[String]
   }
 
-  @Test
-  def verifyEasy() { // Uses ScalaTest assertions
+  @Test def verifyEasy() {
     sb.append("easy!")
     assert(sb.toString === "ScalaTest is easy!")
     assert(lb.isEmpty)
     lb += "sweet"
-    intercept[StringIndexOutOfBoundsException] {
-      "concise".charAt(-1)
-    }
   }
 
-  @Test
-  def pending_test() { // Uses ScalaTest assertions
-	  pending
-  }
-  
-  @Test
-  def verifyFun() { // Uses ScalaTest matchers
+  @Test def verifyFun() {
     sb.append("fun!")
-    sb.toString should be("ScalaTest is fun!")
-    lb should be('empty)
-    lb += "sweet"
-    evaluating { "concise".charAt(-1) } should produce[StringIndexOutOfBoundsException]
-  }
-
-  @Test
-  def scalacheck_call() {
-    check((a: List[Int], b: List[Int]) => a.size + b.size == (a ::: b).size)
+    assert(sb.toString === "ScalaTest is fun!")
+    assert(lb.isEmpty)
   }
 }
